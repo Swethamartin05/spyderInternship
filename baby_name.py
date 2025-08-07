70ce86569a6a14e66411177b76e3a1f58c560dda
swethamartin05 <swethamartin05@gmail.com>
2025-08-07 12:51:27 +0530

    success

---
+++ baby_name.py
@@ -0,0 +1,10 @@
+class baby_name():
+
+    def __init__(self,name):
+        self.n = name
+    
+    def child_name(self):
+        print(f"the daughter of deva and devayani name is:{self.n}")
+
+n = baby_name("devashree")
+n.child_name()
